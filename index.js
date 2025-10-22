c4ab0921b37a8d073a1fdc9f413c95e7abac3a02
ben_mcc17 <194004803+ben-mcc17@users.noreply.github.com>
2025-10-22 10:33:19 -0300

    Add new post data and render functionality; include images and update styles

--- index.js
+++ index.js
@@ -1,30 +1,56 @@
 const posts = [
     {
-        name: "Vincent van Gogh",
-        username: "vincey1853",
-        location: "Zundert, Netherlands",
-        avatar: "images/avatar-vangogh.jpg",
-        post: "images/post-vangogh.jpg",
-        comment: "just took a few mushrooms lol",
-        likes: 21
+        name: "george-springer4",
+        username: "george-springer4",
+        location: "Toronto, Ontario",
+        avatar: "imgs/springer.webp",
+        post: "imgs/Home Run GIF by MLB.gif",
+        caption: "WOOOOOO",
+        likes: "2,546,093"
     },
     {
         name: "Gustave Courbet",
         username: "gus1819",
         location: "Ornans, France",
-        avatar: "images/avatar-courbet.jpg",
-        post: "images/post-courbet.jpg",
-        comment: "i'm feelin a bit stressed tbh",
+        avatar: "imgs/avatar-courbet.jpg",
+        post: "imgs/post-courbet.jpg",
+        caption: "i'm feelin a bit stressed tbh",
         likes: 4
     },
         {
         name: "Joseph Ducreux",
         username: "jd1735",
         location: "Paris, France",
-        avatar: "images/avatar-ducreux.jpg",
-        post: "images/post-ducreux.jpg",
-        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+        avatar: "imgs/avatar-ducreux.jpg",
+        post: "imgs/post-ducreux.jpg",
+        caption: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
         likes: 152
     }
 ]
+
+const postsSection = document.getElementById("posts-section");
+
+function renderPosts() {
+    for (let i=0; i < posts.length; i++) {
+
+    postsSection.innerHTML += `
+    <div class="profile-header">
+        <img class="poster-pic" src="${posts[i].avatar}">
+        <div class="poster-info-container">
+            <p class="poster-username">${posts[i].username}</p>
+            <p class="post-location">${posts[i].location}</p>
+        </div>
+    </div>
+
+    <img src="${posts[i].post}" class="post-img" >
+    <img src="imgs/icon-heart.png" class="icon-img">
+    <img src="imgs/icon-comment.png" class="icon-img">
+    <img src="imgs/icon-dm.png" class="icon-img">
+    <p class="likes-count">${posts[i].likes} likes</p>
+    <p class="caption"><span class="caption-username">${posts[i].username}</span> ${posts[i].caption}</p>
+    `
+    }
+}
+
+renderPosts();
 
